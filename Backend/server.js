b5c490b1c76b3f46bee6fe88ac3abcdec29d403f
priyanshu singh <ishusingh4006@gmail.com>
2025-09-22 10:24:47 +0530

    first commit

---
+++ Backend/server.js
@@ -0,0 +1,12 @@
+const dotenv =require('dotenv');
+dotenv.config();
+
+const http =require('http');
+const app=require('./app');
+
+const server=http.createServer(app);
+
+const PORT=process.env.PORT || 3000;
+server.listen(PORT,()=>{
+    console.log(`serrver started at PORT ${PORT}`);
+});
